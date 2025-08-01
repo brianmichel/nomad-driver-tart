0e7cb0d84f7243c7187adfbc559bcf28229131a6
Brian Michel <brian.michel@gmail.com>
2025-08-01 17:49:45 -0400

    Update registry_host_test.go

--- driver/registry_host_test.go
+++ driver/registry_host_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestRegistryHost(t *testing.T) {
 	cases := map[string]string{
-		"708167139547.dkr.ecr.us-east-2.amazonaws.com/ci-runner-macos-sequoia:latest": "708167139547.dkr.ecr.us-east-2.amazonaws.com",
+		"123474567.dkr.ecr.us-east-2.amazonaws.com/testing-container:latest": "123474567.dkr.ecr.us-east-2.amazonaws.com",
 		"ghcr.io/owner/repo:tag":          "ghcr.io",
 		"https://gcr.io/owner/repo:tag":   "gcr.io",
 		"docker.io/library/ubuntu:latest": "docker.io",
